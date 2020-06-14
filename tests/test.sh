b1d7e237eb216fe73997ac6d3f8796a5f92898bd
yuexiahu <yuexiahu92@hotmail.com>
2020-06-14 20:34:31 +0800

    feat: test.sh add memcheck

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,5 @@ cd $(dirname $0)/..
 cmake -S. -Bbuild -DBUILD_TESTING=ON
 cmake --build build
 cd build
-env CTEST_OUTPUT_ON_FAILURE=1 ctest
+export CTEST_OUTPUT_ON_FAILURE=1
+ctest && ctest -T memcheck
